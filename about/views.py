4e8a54652a99a59fc1a9773b675d9d26605c3c79
Jaime Hyland <james.hyland@iubh.de>
2024-03-07 15:00:34 +0000

    Dev: implement edit and delete comments functionality

--- about/views.py
+++ about/views.py
@@ -1,18 +1,32 @@
-from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
 
 def about(request):
+    if request.method == "POST":
+        print("The site has received a POST request")
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request submitted'
+            )
 
     about = About.objects.all().order_by('updated_on').first()
 
+    collaborate_form = CollaborateForm()
+
     context = {
         "about": about,
+        "collaborate_form": collaborate_form,
     }
 
     return render(
         request, "about/about.html", context,
     )
+
 
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log("The script in the script.js file has run. This text is what should appear on the console!");
